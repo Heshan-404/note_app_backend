117e956f0575dff9cec6c79983ff2299b558ad25
Heshan-404 <heshantharushka2002@gmail.com>
2024-05-28 14:01:19 +0530

    ..

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import express, { Router } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 import notesRouter from "./routes/note";
 import usersRouter from "./routes/user";
+import User from "./models/user"; // Import the User model
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const route = Router();
-// Load environment variables
-require("dotenv").config();
 
 // Connect to MongoDB
 mongoose
@@ -26,7 +27,8 @@ app.use(bodyParser.json());
 
 // Load routes
 app.use("/api/users", usersRouter);
-app.use("/api/users", notesRouter);
+app.use("/api/notes", notesRouter);
+
 app.get("/", (req, res) => {
   res.json({
     name: "sdate",
@@ -37,12 +39,11 @@ app.get("/:userId/notes", async (req, res) => {
   try {
     const { userId } = req.params;
 
-    const user = await User.findOne({ user_id: userId });
+    const user = await User.findOne({ user_id: userId }).populate("notes"); // Use the User model and populate notes
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-
-    res.json(user.notes);
+    res.json(user);
   } catch (error) {
     console.error("Error fetching notes:", error);
     res.status(500).json({ message: "Error fetching notes" });
